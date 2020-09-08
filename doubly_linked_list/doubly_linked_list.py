3ad2d5b796f645a7781d47c2bea6ea9ceb6ec4de
Shaun Lee <shaundlee.0103@gmail.com>
2020-09-08 17:36:31 -0400

    Mod1

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -7,6 +7,9 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
+
+    def __repr__(self):
+        return f'{self.prev}, {self.value}, {self.next}'
             
 """
 Our doubly-linked list class. It holds references to 
@@ -27,7 +30,25 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
+        # create a new node
+        new_node = ListNode(value, None, None)
+        # check if list has content:
+        if not self.head:
+            # if not, we will assign our previous head to current
+            self.head.prev = self.head
+            #and update our head and tail accordingly
+            self.head = new_node
+            self.tail = new_node
+        # otherwise:
+        else:
+            current = self.head
+            print(repr(current))
+            # head's next will be set to the current head
+            self.head.next = current
+            # our head's previous will be updated by our current
+            self.head.prev = new_node
+            # and we set the head as the new node.
+            self.head = new_node
         
     """
     Removes the List's current head node, making the
@@ -35,7 +56,25 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
+        # if there is nothing in the list:
+        if not self.head:
+            return None
+        # else, if there's no items after head
+        if not self.head.next:
+            # make reference to show what is being removed
+            head = self.head
+            # delete the list reference 
+            self.head = None
+            # ensure that the tail has no reference as well
+            self.tail = None
+            # return the referenced value
+            return head.get_value()
+        # and if there is multiple items, get the value at the current head
+        value = self.head
+        # and set head as the next node in the list
+        self.head = self.head.next
+        return value
+
             
     """
     Wraps the given value in a ListNode and inserts it 
@@ -43,7 +82,24 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
+        # create a new node
+        new_node = ListNode(value, None)
+        # check if list has content:
+        if not self.head:
+            # if not, we will assign our previous head to current
+            self.head.prev = self.head
+            #and update our head and tail accordingly
+            self.head = new_node
+            self.tail = new_node
+        # otherwise:
+        else:
+            # we will set our prev value as our current tail,
+            self.tail.prev = self.tail
+            # our current tail will connect to the new node,
+            self.tail.next = new_node
+            # and then we will set the tail as the new node.
+            self.tail = new_node
+
             
     """
     Removes the List's current tail node, making the 
@@ -51,7 +107,24 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        # if there is nothing in the list:
+        if not self.head:
+            return None
+        # otherwise if the reference of head and tail is the same point:
+        if self.head is self.tail:
+            # get a reference where head is
+            value = self.head
+            # and delete the reference for both
+            self.head = None
+            self.tail = None
+            # return the previous reference
+            return value
+        prev = self.tail.prev
+        # otherwise, get a reference to the current tail before removing
+        value = self.tail.get_value()
+        # and set our tail to our tail's prev reference
+        self.tail = prev    
+        return value
             
     """
     Removes the input node from its current spot in the 
--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,47 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     # equivalent of doing len()
+#     def __len__(self):
+#         return self.size
+
+#     # increment the size counter by one and append value onto the end of storage
+#     def push(self, value):
+#         self.size += 1
+#         self.storage.append(value)
+
+#     # decrement the size counter by one and pop value from the end
+#     def pop(self):
+#         # ensure that storage has contents first
+#         if self.size == 0:
+#             return None
+#         self.size -= 1
+#         return self.storage.pop()
+
+
+from singly_linked_list import LinkedList
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-
+        self.storage = LinkedList()
+    
     def __len__(self):
-        pass
+        return self.size
 
+    # add to tail method replaces append
     def push(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size += 1
 
+    #remove tail replaces pop method
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_tail()
